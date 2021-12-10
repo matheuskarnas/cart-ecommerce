b8ff0595ab86773c8af73f06f8f96d10be45f271
matheuskarnas <matheuskarnas1@gmail.com>
2021-12-10 16:51:33 -0300

    utilization LocalStorage in cart

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,6 +1,7 @@
 import { Console, error } from 'console';
 import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
+import { classicNameResolver } from 'typescript';
 import { api } from '../services/api';
 import { Product, Stock } from '../types';
 
@@ -31,22 +32,26 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
   const [cart, setCart] = useState<Product[]>(() => {
 
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem("@RocketShoes:cart")
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
+
+  useEffect(() => {
+    localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart))
+    console.log(cart)
+  }, [cart])
 
   const addProduct = async (productId: number) => {
     try {
       let repetido = false
       // TODO função que vai add product
       for (let i = 0; i < cart.length; i++) {
-        if (cart[i].id === productId) {
-          cart[i].amount = 1
+        if (cart[i].id === productId) {          
           repetido = true;
         }
       }
@@ -83,19 +88,26 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     id,
     amount,
   }: UpdateProductAmount) => {
+
     const checkStock = stock.filter(item => item.id === id)
+
     if (checkStock[0].amount < amount) {
+
       toast.error(`Temos apenas ${checkStock[0].amount} unidades desse item`)
+
     } else if (amount < 1) {
+
       toast.error('valor invalido')
+
     } else {
+
       const newCart = cart.map(product => product.id !== id ? product : {
         ...product,
         amount,
       })
       setCart(newCart)
     }
-    console.log(checkStock)    
+
   }
   return (
     <CartContext.Provider
