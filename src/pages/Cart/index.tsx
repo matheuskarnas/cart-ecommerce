8d951f7816cce41ed44e87edd2a507e6799df753
matheuskarnas <matheuskarnas1@gmail.com>
2021-12-10 18:21:59 -0300

    deletion the CatProduct, content tranfer for Cart

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,7 +1,11 @@
 // import console from 'console';
 import React from 'react';
+import {
+  MdDelete,
+  MdAddCircleOutline,
+  MdRemoveCircleOutline,
+} from 'react-icons/md';
 
-import { CartProduct } from '../../components/CartProduct';
 
 import { useCart } from '../../hooks/useCart';
 import { formatPrice } from '../../util/format';
@@ -11,10 +15,10 @@ import { Product } from '../../types'
 
 
 const Cart = (): JSX.Element => {
-  
+
   const { cart, removeProduct, updateProductAmount } = useCart();
   // const cartFormatted = cart.map(product => ({
-  //   // TODO
+  // TODO
   // }))
   // const total =
   //   formatPrice(
@@ -23,7 +27,7 @@ const Cart = (): JSX.Element => {
   //     }, 0)
   //   )
 
-  const  totalPrice = () => {
+  const totalPrice = () => {
     var total = 0;
     for (let i = 0; i < cart.length; i++) {
       total += cart[i].amount * cart[i].price
@@ -34,15 +38,15 @@ const Cart = (): JSX.Element => {
   function handleProductIncrement({id, amount}: Product) {
     // TODO 
     amount++
-    updateProductAmount({id, amount})
-    
-    
+    updateProductAmount({ id, amount })
+
+
   }
 
-  function handleProductDecrement({id, amount}: Product) {
+  function handleProductDecrement({ id, amount }: Product) {
     // TODO
     amount--
-    updateProductAmount({id, amount})
+    updateProductAmount({ id, amount })
 
   }
 
@@ -63,16 +67,56 @@ const Cart = (): JSX.Element => {
           </tr>
         </thead>
         <tbody>
-          {cart.map(product => <CartProduct
-            id={product.id} 
-            title={product.title} 
-            price={product.price} 
-            image={product.image} 
-            amount={product.amount}
-            handleDelete={handleRemoveProduct}
-            incrementeProduct={handleProductIncrement}
-            decrementProduct={handleProductDecrement}
-          />)}
+          {cart.map(product => {
+            return (
+              <tr data-testid="product" key={product.id}>
+                <td>
+                  <img src={product.image} alt="produto" />
+                </td>
+                <td>
+                  <strong>{product.title}</strong>
+                  <span>{formatPrice(product.price)}</span>
+                </td>
+                <td>
+                  <div>
+                    <button
+                      type="button"
+                      data-testid="decrement-product"
+                      disabled={product.amount <= 1}
+                      onClick={() => handleProductDecrement(product)}
+                    >
+                      <MdRemoveCircleOutline size={20} />
+                    </button>
+                    <input
+                      type="text"
+                      data-testid="product-amount"
+                      readOnly
+                      value={product.amount}
+                    />
+                    <button
+                      type="button"
+                      data-testid="increment-product"
+                      onClick={() => handleProductIncrement(product)}
+                    >
+                      <MdAddCircleOutline size={20} />
+                    </button>
+                  </div>
+                </td>
+                <td>
+                  <strong>{formatPrice(product.amount * product.price)}</strong>
+                </td>
+                <td>
+                  <button
+                    type="button"
+                    data-testid="remove-product"
+                    onClick={() => handleRemoveProduct(product.id)}
+                  >
+                    <MdDelete size={20} />
+                  </button>
+                </td>
+              </tr>
+            )
+          })}
         </tbody>
       </ProductTable>
 
@@ -89,3 +133,14 @@ const Cart = (): JSX.Element => {
 };
 
 export default Cart;
+
+/*<CartProduct
+            id={product.id} 
+            title={product.title} 
+            price={product.price} 
+            image={product.image} 
+            amount={product.amount}
+            handleDelete={handleRemoveProduct}
+            incrementeProduct={handleProductIncrement}
+            decrementProduct={handleProductDecrement}
+          />*/
