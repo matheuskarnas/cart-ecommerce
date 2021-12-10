9d0eab40d13fc7ae7d315b4b74a4bddf9314adc1
matheuskarnas <matheuskarnas1@gmail.com>
2021-12-10 17:49:07 -0300

    logic to disable increment button

--- src/components/CartProduct/index.tsx
+++ src/components/CartProduct/index.tsx
@@ -43,7 +43,7 @@ export function CartProduct({
                     <button
                         type="button"
                         data-testid="decrement-product"
-                        // disabled={amount <= 1}
+                        disabled={amount <= 1}
                         onClick={() => decrementProduct({ id, title, price, image, amount })}
                     >
                         <MdRemoveCircleOutline size={20} />
